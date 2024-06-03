3f3031907e00bc38371bb631b5b05406fde0b6a6
momaek <momaek17@gmail.com>
2024-06-03 23:59:27 +0800

    add list roles api

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,11 +1,19 @@
 import request from '@/utils/axios'
-import type { Chat } from '@/models/chat'
+import type { Chat, Role } from '@/models/chat'
 
 export async function getChatList(page?: number, limit?: number): Promise<Chat[]> {
   return request
     .get<Chat[]>({
       url: '/chat/list',
       params: { page: page, page_size: limit }
+    })
+    .then((res) => res.data)
+}
+
+export async function getRoleList(): Promise<Role[]> {
+  return request
+    .get<Role[]>({
+      url: '/role/list'
     })
     .then((res) => res.data)
 }
--- src/models/chat.ts
+++ src/models/chat.ts
@@ -15,3 +15,37 @@ export interface UpdateChatParam {
   chat_id: string
   title: string
 }
+
+export interface Model {
+  id: number
+  created_at: number
+  updated_at: number
+  platform: string
+  name: string
+  value: string
+  enabled: boolean
+  sort_num: number
+  power: number
+  open: boolean
+  max_tokens: number
+  max_context: number
+  temperature: number
+  key_id: number
+  key_name: string
+}
+
+export interface Role {
+  id: number
+  created_at: number
+  updated_at: number
+  key: string
+  name: string
+  context: any
+  hello_msg: string
+  icon: string
+  enable: boolean
+  sort: number
+  model_id: number
+  model_name: string
+  description?: string
+}
